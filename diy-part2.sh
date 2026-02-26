d1b185e5e83d94c243375dff1827f806beacd6c2
zikura88 <982535189@qq.com>
2026-02-26 08:26:03 +0800

    Change default IP address from 192.168.1.1 to 192.168.5.1

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,4 +8,5 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
